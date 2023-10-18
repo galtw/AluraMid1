f10583cec8c47c05581323b0b1d2a3cc3a12ec30
Graziela Larissa <111892130+galtw@users.noreply.github.com>
2023-10-18 08:16:17 -0300

    Add files via upload

---
+++ AluraMid1-main/AluraMid-main/main.js
@@ -0,0 +1,40 @@
+function tocaSom(){
+    document.querySelector (idElementoAudio) .play('');
+    document.querySelector (idElementoAudio).onclick = tocaSomPom ();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+for (contador = 0; contador < listaDeTeclas.lenght; contador++);
+    const instrumento = listaDeTeclas[contador];
+    const tecla = listaDeTeclas[contador];
+    const idAudio = `#som_${instrumento}`;
+    listaDeTeclas[contador].classlist [1];
+
+    tecla.onclick = tocaSomPom;
+    tocaSom ('#som_tecla_Pom');
+    tecla.onclick = tocaSomClap;
+    tocaSom ('#som_tecla_Clap');
+    tecla.onclick = tocaSomTim;
+    tocaSom ('#som_tecla_Tim');
+    tecla.onclick = tocaSomPuff;
+    tocaSom ('#som_tecla_Puff');
+    tecla.onclick = tocaSomSplash;
+    tocaSom ('#som_tecla_Splash');
+    tecla.onclick = tocaSomToim;
+    tocaSom ('#som_tecla_Toim');
+    tecla.onclick = tocaSomPsh;
+    tocaSom ('#som_tecla_Psh');
+    tecla.onclick = tocaSomTic;
+    tocaSom ('#som_tecla_Tic');
+    tecla.onclick = tocaSomTom;
+    tocaSom ('#som_tecla_Tom');
+    
+listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
+listaDeTeclas[4].onclick = tocaSomSplash;
+listaDeTeclas[5].onclick = tocaSomToim;
+listaDeTeclas[6].onclick = tocaSomPsh;
+listaDeTeclas[7].onclick = tocaSomTic;
+listaDeTeclas[8].onclick = tocaSomTom;
